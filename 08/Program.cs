e43938e79f43d214369af62ebae1b3ac25da2bb5
Ludde <ludde@tunel.se>
2016-03-25 19:25:01 +0100

    Day 8 done

--- 08/Program.cs
+++ 08/Program.cs
@@ -18,19 +18,21 @@ namespace _08
 
 			var totalActualCharacters = input.Sum(NumberOfCharacters);
 			var totalLiteralCharacters = input.Sum(line => line.Length);
+			var escapedLength = input.Sum(str => EscapeString(str).Length + 2);
 
-			var result = totalLiteralCharacters - totalActualCharacters;
+			var result1 = totalLiteralCharacters - totalActualCharacters;
+			var result2 = escapedLength - totalLiteralCharacters;
 
-			Console.WriteLine(result);
+			Console.WriteLine(result1);
+			Console.WriteLine(result2);
 			Console.ReadLine();
 		}
 
 		private static Regex s_regex = new Regex(@"\\\\|\\\""|\\x[0-9a-f]{2}", RegexOptions.Compiled);
-
 		public static int NumberOfCharacters(string input)
 		{
 			if (!input.StartsWith("\"") || !input.EndsWith("\""))
-				throw new ArgumentException("String is no good", nameof(input));
+				throw new ArgumentException($"String is no good: [{input}]", nameof(input));
 
 			input = input.Substring(1, input.Length - 2);
 
@@ -39,6 +41,24 @@ namespace _08
 			var matchCount = matches.Count();
 			
 			return input.Length - matchLengths + matchCount;
+		}
+
+		public static string EscapeString(string input)
+		{
+			var replacements = new Dictionary<string, string>()
+			{
+				{ @"""", @"\""" }, // " -> \"
+				{ @"\", @"\\" }, // \ -> \\"
+			};
+
+			var regexPattern = string.Join("|", replacements.Keys.Select(k => Regex.Escape(k)));
+
+			var regex = new Regex(regexPattern);
+
+			return regex.Replace(input, (Match m) =>
+			{
+				return replacements[m.Value];
+			});
 		}
 	}
 
@@ -55,5 +75,22 @@ namespace _08
 		{
 			return Program.NumberOfCharacters(input);
 		}
+
+		[TestCase("\"asdsdasd")]
+		[TestCase("asdsdasd\"")]
+		public void Test_NumberOfCharacters_ThrowsOnNonEnclosedString(string input)
+		{
+			Assert.Throws<ArgumentException>(() => Program.NumberOfCharacters(input));
+		}
+
+		[TestCase(@"""", ExpectedResult = @"\""")]
+		[TestCase(@"\", ExpectedResult = @"\\")]
+		[TestCase(@"\x23", ExpectedResult = @"\\x23")]
+		[TestCase(@"ab\""ab""ab", ExpectedResult =@"ab\\\""ab\""ab")]
+		public string Test_EscapeString(string input)
+		{
+			return Program.EscapeString(input);
+		}
+
 	}
 }
