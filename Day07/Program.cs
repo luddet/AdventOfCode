0f5db3eaf764f589dc8fb9d204b9596325e1293b
Ludde <github@tunel.se>
2016-12-15 15:21:17 +0100

    Day07 part 2 failing

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -15,14 +16,19 @@ namespace Day07
 
 		// matchar ABBA som har [] runt sig
 		private static readonly Regex s_bracketedAbbaMatcher = new Regex(@"(?<=\[\w*)(\w)(?!\1)(\w)\2\1(?=\w*\])", RegexOptions.Compiled);
+		private static readonly Regex s_sslMatcher = new Regex(@"\[\w*(\w)(?!\1)(\w)\1\w*\].*?\2\1\2|(\w)(?!\3)(\w)\3.*?\[[^]]*\4\3\4.*\]", RegexOptions.Compiled);
 
 		static void Main(string[] args)
 		{
 			var input = File.ReadAllLines("input.txt");
 
 			var count = input.Count(line => s_abbaMatcher.IsMatch(line) && !s_bracketedAbbaMatcher.IsMatch(line));
+			int sslCount = 0;
+			foreach (var line in input.Where(line => s_sslMatcher.IsMatch(line)))
+				++sslCount;
 
 			Console.WriteLine(count);
+			Console.WriteLine("sslcount: {0}", sslCount);
 			Console.ReadLine();
 		}
 	}
