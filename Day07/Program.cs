71f1565915811d801e5a488164d8c4abb13b717e
Ludde <github@tunel.se>
2016-12-15 14:30:09 +0100

    Day07 Part 1 Complete

---
+++ Day07/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day07
+{
+	class Program
+	{
+		// matchar ABBA 
+		private static readonly Regex s_abbaMatcher = new Regex(@"(\w)(?!\1)(\w)\2\1", RegexOptions.Compiled);
+
+		// matchar ABBA som har [] runt sig
+		private static readonly Regex s_bracketedAbbaMatcher = new Regex(@"(?<=\[\w*)(\w)(?!\1)(\w)\2\1(?=\w*\])", RegexOptions.Compiled);
+
+		static void Main(string[] args)
+		{
+			var input = File.ReadAllLines("input.txt");
+
+			var count = input.Count(line => s_abbaMatcher.IsMatch(line) && !s_bracketedAbbaMatcher.IsMatch(line));
+
+			Console.WriteLine(count);
+			Console.ReadLine();
+		}
+	}
+}
