9b20e992ee03b45caa8b36d746b9821e8adafd11
luddet <ludde@tunel.se>
2017-12-07 16:19:26 +0100

    Day05 Complete

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,38 @@ namespace Day05
 {
 	class Program
 	{
+		private static int[] s_testInput = { 0, 3, 0, 1, -3 };
+
 		static void Main(string[] args)
 		{
+			var input = File.ReadAllLines("input.txt").Where(str => !string.IsNullOrEmpty(str)).Select(int.Parse).ToArray();
+			Console.WriteLine("Part 1: " + Execute_Part1(input));
+			Console.WriteLine("Part 2: " + Execute_Part2(input));
+			Console.ReadLine();
+		}
+
+		private static int Execute_Part1(int[] instructions)
+		{
+			instructions = instructions.ToArray();
+			int ip = 0, ic = 0;
+			while (ip >= 0 && ip < instructions.Length)
+			{
+				ip += instructions[ip]++;
+				++ic;
+			}
+			return ic;
+		}
+
+		private static int Execute_Part2(int[] instructions)
+		{
+			instructions = instructions.ToArray();
+			int ip = 0, ic = 0;
+			while (ip >= 0 && ip < instructions.Length)
+			{
+				ip += instructions[ip] >= 3 ? instructions[ip]-- : instructions[ip]++;
+				++ic;
+			}
+			return ic;
 		}
 	}
 }
