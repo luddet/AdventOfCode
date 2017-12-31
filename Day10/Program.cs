258b6b6ac4cd19ad1e64287a32e3095438661720
luddet <ludde@tunel.se>
2017-12-31 02:03:08 +0100

    Day10 Part 2 Complete

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -12,50 +12,120 @@ namespace Day10
 	{
 		static void Main(string[] args)
 		{
-			var input = File.ReadAllText("input.txt").Split(',').Select(Int32.Parse).ToArray();
-			var hash = Hash(input);
+			var input = File.ReadAllText("input.txt").Trim();
 
-			Console.WriteLine($"Part 1: {hash}");
+			Part1(input);
+			Part2(input);
+
 			Console.ReadLine();
 		}
 
-		public static int Hash(int[] seed, int bufferSize = 256)
+		private static void Part1(string inputString)
 		{
-			int pos = 0;
-			int skip = 0;
+			var input = inputString.Split(',').Select(int.Parse).ToArray();
+			var hash = new Hasher().SingleHash(input);
+			Console.WriteLine($"Part 1: {hash}");
+		}
 
-			var buffer = new int[bufferSize];
-			for (var i = 0; i < bufferSize; ++i)
-				buffer[i] = i;
+		private static void Part2(string inputString)
+		{
+			var input = inputString.Select(b => (int)b).ToArray();
+			var hash = new Hasher().Hash(input);
+			Console.WriteLine($"Part 2: {hash}");
 
+		}
+	}
 
-			foreach (var length in seed)
+	class Hasher
+	{
+		private static readonly int[] s_suffix = {17, 31, 73, 47, 23};
+		private readonly int[] m_buffer;
+		private int m_pos;
+		private int m_skip;
+
+		public Hasher(int size = 256)
+		{
+			m_buffer = new int[size];
+			for (var i = 0; i < size; ++i)
+				m_buffer[i] = i;
+
+			m_pos = 0;
+			m_skip = 0;
+		}
+
+		public string Hash(int[] seed)
+		{
+			var newSeed = new int[seed.Length + s_suffix.Length];
+			seed.CopyTo(newSeed, 0);
+			s_suffix.CopyTo(newSeed, seed.Length);
+
+			for (int i = 0; i < 64; ++i)
+				SingleHash(newSeed);
+
+			var denseHash = new int[16];
+			for (int i = 0; i < 16; ++i)
 			{
-				var lastIndex = pos + length - 1;
-				for (int i = 0; i < length/2; ++i)
-				{
-					var tmp = buffer[(pos + i) % buffer.Length];
-					buffer[(pos + i) % buffer.Length] = buffer[(lastIndex - i) % buffer.Length];
-					buffer[(lastIndex - i) % buffer.Length] = tmp;
-				}
-
-				pos = (pos + length + skip) % buffer.Length;
-				++skip;
+				var xor = m_buffer[i * 16];
+				for (int j = i * 16 + 1; j < i * 16 + 16; ++j)
+					xor ^= m_buffer[j];
+				denseHash[i] = xor;
 			}
 
-			return buffer[0] * buffer[1];
+			var sb = new StringBuilder(32);
+			foreach (var n in denseHash)
+				sb.AppendFormat("{0:X2}", n);
+
+			return sb.ToString().ToLower();
+		}
+
+		public int SingleHash(int[] seed)
+		{
+			foreach (var length in seed)
+			{
+				var lastIndex = m_pos + length - 1;
+				for (int i = 0; i < length/2; ++i)
+				{
+					var tmp = m_buffer[(m_pos + i) % m_buffer.Length];
+					m_buffer[(m_pos + i) % m_buffer.Length] = m_buffer[(lastIndex - i) % m_buffer.Length];
+					m_buffer[(lastIndex - i) % m_buffer.Length] = tmp;
+				}
+
+				m_pos = (m_pos + length + m_skip) % m_buffer.Length;
+				++m_skip;
+			}
+
+			return m_buffer[0] * m_buffer[1];
 		}
 	}
 
 	[TestFixture]
-	class Tests
+	class HasherTests
 	{
 		[Test]
-		public void Hash_ReturnsCorrectHash()
+		public void SingleHash_ReturnsCorrectHash()
 		{
 			var seed = new[] { 3, 4, 1, 5 };
-			var hash = Program.Hash(seed,5);
+			var hash = new Hasher(5).SingleHash(seed);
 			Assert.That(hash, Is.EqualTo(12));
+		}
+
+		private static IReadOnlyList<Tuple<string, string>> s_hashes = new List<Tuple<string, string>>
+		{
+			Tuple.Create("", "a2582a3a0e66e6e86e3812dcb672a272"),
+			Tuple.Create("AoC 2017", "33efeb34ea91902bb2f59c9920caa6cd"),
+			Tuple.Create("1,2,3", "3efbe78a8d82f29979031a4aa0b16a9d"),
+			Tuple.Create("1,2,4", "63960835bcdc130f0b66d7ff4f6a5a8e"),
+		};
+
+		[TestCaseSource(nameof(s_hashes))]
+		public void Hash(Tuple<string, string> hash)
+		{
+			var expectedResult = hash.Item2;
+			var inputString = hash.Item1;
+			var inputArray = inputString.Select(c => (int) c).ToArray();
+			var result = new Hasher().Hash(inputArray);
+
+			Assert.That(result, Is.EqualTo(expectedResult));
 		}
 	}
 }
