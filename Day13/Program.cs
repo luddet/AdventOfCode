0bbeffd5d8b6943cbf044d7ee4679771dd7d5310
luddet <ludde@tunel.se>
2018-01-01 15:14:05 +0100

    Day13 Setup

---
+++ Day13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13
+{
+	class Program
+	{
+		private static readonly string[] s_testInput =
+		{
+			"0: 3",
+			"1: 2",
+			"4: 4",
+			"6: 4"
+		};
+
+		static void Main(string[] args)
+		{
+
+
+
+		}
+	}
+}
