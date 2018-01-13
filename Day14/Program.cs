d699f2bea5b1f301778c8fe6a5dd2aebc7dd0e00
luddet <ludde@tunel.se>
2018-01-13 01:04:09 +0100

    Day14 Setup

---
+++ Day14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14
+{
+	class Program
+	{
+		private const string INPUT = "amgozmfv";
+		private const string TEST_INPUT = "flqrgnkx";
+
+		static void Main(string[] args)
+		{
+		}
+	}
+
+	
+}
