e527fcd86c02394b60d0121666bd70dbd17922f0
luddet <ludde@tunel.se>
2018-01-14 14:38:54 +0100

    Day17 Setup

---
+++ Day17/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day17
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const int input = 356;
+		}
+	}
+}
