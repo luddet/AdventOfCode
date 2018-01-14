1b5e32afc5d411f1be9e5168de1bf8888054c10e
luddet <ludde@tunel.se>
2018-01-14 14:17:47 +0100

    Day16 Complete Added loop detection.

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -27,127 +27,53 @@ namespace Day16
 			'e'
 		};
 
-		private interface IOperation
-		{
-			void Execute(DanceContext context);
-		}
-
-		private class SpinOperation : IOperation
-		{
-			private readonly int m_steps;
-
-			public SpinOperation(int steps)
-			{
-				m_steps = steps;
-			}
-
-			public void Execute(DanceContext context)
-			{
-				context.Spin = (context.Spin + m_steps) % context.Buffer.Length;
-			}
-		}
-
-		private class ExchangeOperation : IOperation
-		{
-			private readonly int m_b;
-			private readonly int m_a;
-
-			public ExchangeOperation(int a, int b)
-			{
-				m_a = a;
-				m_b = b;
-			}
-
-			public void Execute(DanceContext context)
-			{
-				var indexA = (m_a - context.Spin + context.Buffer.Length) % context.Buffer.Length;
-				var indexB = (m_b - context.Spin + context.Buffer.Length) % context.Buffer.Length;
-
-				var tmp = context.Buffer[indexA];
-				context.Buffer[indexA] = context.Buffer[indexB];
-				context.Buffer[indexB] = tmp;
-			}
-		}
-
-		private class PartnerOperation : IOperation
-		{
-			private readonly char m_a;
-			private readonly char m_b;
-
-			public PartnerOperation(char a, char b)
-			{
-				m_a = a;
-				m_b = b;
-			}
-
-			public void Execute(DanceContext context)
-			{
-				int aIndex = -1;
-				int bIndex = -1;
-				for (int i = 0; i < context.Buffer.Length; ++i)
-				{
-					if (context.Buffer[i] == m_a)
-					{
-						aIndex = i;
-						if (bIndex != -1)
-							break;
-					}
-					else if (context.Buffer[i] == m_b)
-					{
-						bIndex = i;
-						if (aIndex != -1)
-							break;
-					}
-				}
-
-				var tmp = context.Buffer[aIndex];
-				context.Buffer[aIndex] = context.Buffer[bIndex];
-				context.Buffer[bIndex] = tmp;
-			}
-		}
-
-		private class DanceContext
-		{
-			public DanceContext(char[] buffer)
-			{
-				Buffer = new char[buffer.Length];
-				buffer.CopyTo(Buffer, 0);
-				Spin = 0;
-			}
-
-			public int Spin { get; set; }
-			public readonly char[] Buffer;
-		}
 
 		static void Main(string[] args)
 		{
 			var buffer = new char[16] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'};
 			var input = File.ReadAllText("input.txt").Split(new []{','}, StringSplitOptions.RemoveEmptyEntries);
-			
-//			buffer = s_testBuffer;
-//			input = s_testInput;
+			var iterations = 1000000000;
+
+			// Test overrides
+			//			iterations = 2;
+			//			buffer = s_testBuffer;
+			//			input = s_testInput;
 
 			var context = new DanceContext(buffer);
 
 			var firstBufferState = new char[buffer.Length];
 			var operations = ParseOperations(input);
 
-			var iterations = 1000000000;
-//			iterations = 2;
+			var states = new Dictionary<DanceContext, int>();
+
 			for (int i = 0; i < iterations; ++i)
 			{
 				foreach (var operation in operations)
 					operation.Execute(context);
 
+				if (states != null)
+				{
+					if (!states.ContainsKey(context))
+					{
+						states.Add(context.DeepClone(), i);
+					}
+					else // Found loop
+					{
+						states.TryGetValue(context, out int loopStart);
+						var loopEnd = i;
+						var loopPeriod = i - loopStart;
+						var loopSkips = (iterations - loopStart) / loopPeriod;
+						i = loopSkips * loopPeriod + loopStart;
+						states = null;
+						Console.WriteLine($"Found loop from {loopStart} to {loopEnd}, length: {loopPeriod}, finishing up from: {i}");
+
+					}
+				}
+
 				if (i == 0)
 				{
 					context.Buffer.CopyTo(firstBufferState, 0);
 					RotateBuffer(context.Spin, firstBufferState);
-				}
-
-				if (i % 100000 == 0)
-				{
-					Console.WriteLine($"{100*(float)i/iterations}%");
 				}
 			}
 
@@ -196,4 +122,126 @@ namespace Day16
 				buffer[(i + steps) % bufferSize] = temp[i];
 		}
 	}
+
+	internal interface IOperation
+	{
+		void Execute(DanceContext context);
+	}
+
+	internal class SpinOperation : IOperation
+	{
+		private readonly int m_steps;
+
+		public SpinOperation(int steps)
+		{
+			m_steps = steps;
+		}
+
+		public void Execute(DanceContext context)
+		{
+			context.Spin = (context.Spin + m_steps) % context.Buffer.Length;
+		}
+	}
+
+	internal class ExchangeOperation : IOperation
+	{
+		private readonly int m_b;
+		private readonly int m_a;
+
+		public ExchangeOperation(int a, int b)
+		{
+			m_a = a;
+			m_b = b;
+		}
+
+		public void Execute(DanceContext context)
+		{
+			var indexA = (m_a - context.Spin + context.Buffer.Length) % context.Buffer.Length;
+			var indexB = (m_b - context.Spin + context.Buffer.Length) % context.Buffer.Length;
+
+			var tmp = context.Buffer[indexA];
+			context.Buffer[indexA] = context.Buffer[indexB];
+			context.Buffer[indexB] = tmp;
+		}
+	}
+
+	internal class PartnerOperation : IOperation
+	{
+		private readonly char m_a;
+		private readonly char m_b;
+
+		public PartnerOperation(char a, char b)
+		{
+			m_a = a;
+			m_b = b;
+		}
+
+		public void Execute(DanceContext context)
+		{
+			int aIndex = -1;
+			int bIndex = -1;
+			for (int i = 0; i < context.Buffer.Length; ++i)
+			{
+				if (context.Buffer[i] == m_a)
+				{
+					aIndex = i;
+					if (bIndex != -1)
+						break;
+				}
+				else if (context.Buffer[i] == m_b)
+				{
+					bIndex = i;
+					if (aIndex != -1)
+						break;
+				}
+			}
+
+			var tmp = context.Buffer[aIndex];
+			context.Buffer[aIndex] = context.Buffer[bIndex];
+			context.Buffer[bIndex] = tmp;
+		}
+	}
+
+	internal class DanceContext
+	{
+		public DanceContext(char[] buffer)
+		{
+			Buffer = new char[buffer.Length];
+			buffer.CopyTo(Buffer, 0);
+			Spin = 0;
+		}
+
+		public int Spin { get; set; }
+		public readonly char[] Buffer;
+
+		public override int GetHashCode()
+		{
+			int hash = 17;
+			hash = hash * 31 + Spin;
+			hash = hash * 31 + string.Join("", Buffer).GetHashCode();
+			return hash;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj == null)
+				return false;
+
+			if (ReferenceEquals(this, obj))
+				return true;
+
+			if (GetType() != obj.GetType())
+				return false;
+
+			var other = obj as DanceContext;
+			return other != null && Spin == other.Spin && Buffer.SequenceEqual(other.Buffer);
+		}
+
+		public DanceContext DeepClone()
+		{
+			return new DanceContext(Buffer) { Spin = this.Spin };
+		}
+	}
+
+
 }
