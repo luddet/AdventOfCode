3b0ffe474134e42d939c873e8e5aaecdf4e68d84
luddet <ludde@tunel.se>
2018-01-14 15:34:26 +0100

    Day17 Part 1 Complete

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -10,7 +10,47 @@ namespace Day17
 	{
 		static void Main(string[] args)
 		{
-			const int input = 356;
+			const int steps = 356;
+			const int totalValues = 2017; // 50000000;
+
+			Node zeroNode = new Node(0);
+			zeroNode.Next = zeroNode;
+
+			int idAfter2017 = 0;
+
+			var currentNode = zeroNode;
+			for (int i = 1; i <= totalValues; ++i)
+			{
+				for (int j = 0; j < steps; ++j)
+					currentNode = currentNode.Next;
+
+				var newNode = new Node(i, currentNode.Next);
+				currentNode = currentNode.Next = newNode;
+
+				if (i == 2017)
+					idAfter2017 = currentNode.Next.Id;
+
+				if (i % 100000 == 0)
+					Console.WriteLine(i);
+			}
+
+			int idAfterZero = zeroNode.Next.Id;
+
+			Console.WriteLine($"Part 1: {idAfter2017}");
+			//Console.WriteLine($"Part 2: {idAfterZero}");
+			Console.ReadLine();
+		}
+	}
+
+	class Node
+	{
+		public int Id { get; }
+		public Node Next { get; set; }
+
+		public Node(int id, Node next = null)
+		{
+			Id = id;
+			Next = next;
 		}
 	}
 }
