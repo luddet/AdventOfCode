4c66d5c4c01a24446b53d69c8ed763100b76b495
luddet <ludde@tunel.se>
2018-12-02 16:37:54 +0100

    Day01 Part 2 complete

--- Day01/Day01.cs
+++ Day01/Day01.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -32,14 +33,20 @@ namespace Day01
 		}
 
 		[Test]
-		
 		public void ParseInput_InvalidInput_Throws()
 		{
 			var invalidInput = new string[] { "abca", "-2", "+3" };
 
 			Assert.That(() => { Day01.ParseInput(invalidInput).ToList(); }, Throws.TypeOf<FormatException>());
+		}
 
-
+		[TestCase(new[] {+1, -1}, ExpectedResult = 0)]
+		[TestCase(new []{+3, +3, +4, -2, -4}, ExpectedResult = 10)]
+		[TestCase(new []{-6, +3, +8, +5, -6}, ExpectedResult = 5)]
+		[TestCase(new[] { +7, +7, -2, -7, -4}, ExpectedResult = 14)]
+		public int FindFirstDuplicateResult(int[] input)
+		{
+			return Day01.FindFirstDuplicateResult(input);
 		}
 	}
 
@@ -49,9 +56,11 @@ namespace Day01
 		static void Main(string[] args)
 		{
 			var input = File.ReadLines("input.txt");
-			var corrections = ParseInput(input);
-			var result = CalibrateFrequencyDrift(corrections);
-			Console.WriteLine($"Day01 part 1 result: {result}");
+			var corrections = ParseInput(input).ToArray();
+			var part1Result = CalibrateFrequencyDrift(corrections);
+			var part2Result = FindFirstDuplicateResult(corrections);
+			Console.WriteLine($"Day01 part 1 result: {part1Result}");
+			Console.WriteLine($"Day01 part 2 result: {part2Result}");
 			Console.ReadLine();
 		}
 
@@ -63,6 +72,20 @@ namespace Day01
 		public static IEnumerable<int> ParseInput(IEnumerable<string> input)
 		{
 			return input.Select(str => int.Parse(str));
+		}
+
+		public static int FindFirstDuplicateResult(int[] input)
+		{
+			HashSet<int> results = new HashSet<int>();
+
+			int currentResult = 0;
+			int index = 0;
+			while (results.Add(currentResult))
+			{
+				currentResult += input[index];
+				index = ++index % input.Length;
+			}
+			return currentResult;
 		}
 	}
 }
