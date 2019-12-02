000b27f572ca2f11a0ea13dd30f198150c738202
luddet <ludde@tunel.se>
2019-12-02 23:03:44 +0100

    Day02 Part 2 done

--- AdventOfCode2019/Day02/Day02.cpp
+++ AdventOfCode2019/Day02/Day02.cpp
@@ -2,44 +2,73 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-//#include <exception>
+
+void runProgram(std::vector<int>& program)
+{
+	size_t instructionStride = 4;
+	int opCode = 0;
+	for (size_t ip = 0; ip < program.size() && opCode != 99; ip += instructionStride)
+	{
+		opCode = program[ip];
+		switch (opCode)
+		{
+		case 1:
+			program[program[ip + 3]] = program[program[ip + 1]] + program[program[ip + 2]];
+			break;
+		case 2:
+			program[program[ip + 3]] = program[program[ip + 1]] * program[program[ip + 2]];
+			break;
+		case 99:
+			break;
+		default:
+			throw std::exception("Unhandled opcode", opCode);
+		}
+	}
+}
 
 int main()
 {
 	std::ifstream fs("input.txt");
-	std::vector<int> program;
+	std::vector<int> originalMemory;
 	int i;
 	while (fs >> i)
 	{
-		program.push_back(i);
+		originalMemory.push_back(i);
 		char comma;
 		fs >> comma; // eat comma
 	}
 
-	// patch program
-	program[1] = 12;
-	program[2] = 2;
+	std::vector<int> part1Memory(originalMemory);
+	part1Memory[1] = 12;
+	part1Memory[2] = 2;
+	runProgram(part1Memory);
 
-	size_t instructionStride = 4;
-	int opCode = 0;
-	for (size_t pc = 0; pc < program.size() && opCode != 99; pc += instructionStride)
+	std::cout << "Part 1: " << part1Memory[0] << std::endl;
+
+	bool done = false;
+	size_t noun, verb;
+	for (noun = 0; noun <= 99; ++noun)
 	{
-		opCode = program[pc];
-		switch (opCode)
+		for (verb = 0; verb <= 99; ++verb)
 		{
-			case 1:
-				program[program[pc + 3]] = program[program[pc + 1]] + program[program[pc + 2]];
+			std::vector<int> part2Memory(originalMemory);
+			part2Memory[1] = noun;
+			part2Memory[2] = verb;
+			runProgram(part2Memory);
+			done = part2Memory[0] == 19690720;
+			if (done)
 				break;
-			case 2:
-				program[program[pc + 3]] = program[program[pc + 1]] * program[program[pc + 2]];
-				break;
-			case 99:
-				break;
-			default:
-				throw std::exception("Unhandled opcode", opCode);
 		}
+		if (done)
+			break;
 	}
 
-	std::cout << "Part 1: " << program[0] << std::endl;
+	if (!done)
+	{
+		std::cout << "No solution found." << std::endl;
+		return 1;
+	}
+
+	std::cout << "Part 2: " << (noun * 100 + verb) << std::endl;
 }
 
