75f495e2e21fb3292bd56b6f759b969b3d824678
luddet <luddet@users.noreply.github.com>
2019-12-09 16:27:35 +0100

    Update Day06.cpp
    Day06 part 2 complete

--- AdventOfCode2019/Day06/Day06.cpp
+++ AdventOfCode2019/Day06/Day06.cpp
@@ -4,7 +4,6 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <cassert>
 #include <unordered_map>
 
 using orbit_map = std::unordered_map<std::string, std::string>;
@@ -15,7 +14,6 @@ orbit_map buildTree(std::vector<std::string>& input)
     for (auto line : input)
     {   
         size_t delimIndex = line.find(')');
-        assert(delimIndex != std::string::npos);
         objects.insert(std::make_pair(line.substr(delimIndex + 1), line.substr(0, delimIndex)));
     }
 
@@ -38,6 +36,26 @@ uint32_t orbitCount(const orbit_map& map)
     return count;
 }
 
+std::vector<std::string> getRootedPath(const std::string& startNode, const orbit_map& map)
+{
+	std::vector<std::string> path;
+	auto currentNode = map.find(startNode);
+	while (currentNode != map.end())
+	{
+		path.push_back(currentNode->first);
+		auto nextNode = map.find(currentNode->second);
+		
+		if (nextNode == map.end()) // Found root node
+			path.push_back(currentNode->second);
+
+		currentNode = nextNode;
+	}
+
+	std::reverse(std::begin(path), std::end(path));
+
+	return path;
+}
+
 int main()
 {
     std::fstream fs("input.txt");
@@ -49,5 +67,19 @@ int main()
     orbit_map map = buildTree(input);
     uint32_t count = orbitCount(map);
 
-    std::cout << "Part 1: " << count << std::endl;
+	auto youPath = getRootedPath("YOU", map);
+	auto sanPath = getRootedPath("SAN", map);
+
+	// Find index of first common ancestor. We assume that there is always at least a common root node.
+	uint32_t cai = 0;
+	while (youPath[cai] == sanPath[cai])
+		++cai;
+	--cai;
+
+	// subtract common path length and 1 for the common node and 1 for the node is, times 2 and for both paths
+	uint32_t jumps = (youPath.size() + sanPath.size() - cai - cai - 4);
+
+	std::cout << "Part 1: " << count << std::endl;
+	std::cout << "Part 2: " << jumps << std::endl;
+
 }
