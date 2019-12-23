1299de21113d9de5d4b5895a3e21c5b60ed6e77b
luddet <luddet@users.noreply.github.com>
2019-12-23 12:30:18 +0100

    Day13 Part 1 Complete

---
+++ AdventOfCode2019/Day13/Computer.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <vector>
+#include <queue>
+
+class Computer
+{
+public:
+	using mem_item_t = long long;
+	using mem_container_t = std::vector<mem_item_t>;
+	enum class ComputerState
+	{
+		IDLE,
+		WAITING_FOR_INPUT,
+		DONE,
+	};
+
+	Computer(const mem_container_t& memory);
+	void addInput(mem_item_t input);
+	bool getOutput(mem_item_t& output);
+	ComputerState getState() const { return m_state; }
+	bool isDone() const;
+	void run();
+
+private:
+	enum class Mode
+	{
+		POSITION = 0,
+		IMMEDIATE = 1,
+		RELATIVE = 2
+	};
+
+	mem_item_t getMemValue(Mode mode, size_t address);
+	void setMemValue(mem_item_t value, size_t address, Mode mode);
+	void resizeMemory(size_t newSize);
+
+	size_t				m_ip; // instruction pointer
+	size_t				m_relativeBase;
+	mem_container_t	m_memory;
+	std::queue<mem_item_t>		m_input;
+	std::queue<mem_item_t>		m_output;
+	ComputerState		m_state;
+};
+
+/// Reads comma separated list of opcodes
+Computer::mem_container_t readMemory(std::istream& stream);
--- AdventOfCode2019/Day13/Day13.cpp
+++ AdventOfCode2019/Day13/Day13.cpp
@@ -2,8 +2,66 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <map>
+
+#include "Computer.h"
+
+struct coord_t
+{
+	int x;
+	int y;
+};
+coord_t& operator+=(coord_t& lhs, const coord_t& rhs)
+{
+	lhs.x += rhs.x;
+	lhs.y += rhs.y;
+	return lhs;
+}
+
+bool operator<(const coord_t& lhs, const coord_t& rhs)
+{
+	return (lhs.x < rhs.x) ? true : (lhs.x == rhs.x && lhs.y < rhs.y);
+}
+
+enum class Tile : uint8_t
+{
+	Empty = 0,
+	Wall,
+	Block,
+	HorizontalPaddle,
+	Ball,
+	TiletypesCount
+};
 
 int main()
 {
-  
+	std::fstream fs("input.txt");
+	auto mem = readMemory(fs);
+	Computer c(mem);
+
+	while (!c.isDone())
+		c.run();
+
+	std::map<coord_t, Tile> tileMap;
+
+	Computer::mem_item_t x, y, tile_id;
+	while (c.getOutput(x))
+	{
+		c.getOutput(y);
+		c.getOutput(tile_id);
+
+		coord_t coord{ (int)x, (int)y };
+		auto it = tileMap.find(coord);
+		if (it == tileMap.end())
+			tileMap.insert(std::make_pair(coord, (Tile)tile_id));
+		else
+			it->second = (Tile)tile_id;
+	}
+
+	auto blocks = std::count_if(tileMap.begin(), tileMap.end(), [](auto p) { return p.second == Tile::Block; });
+
+	std::cout << "Part 1: " << blocks << std::endl;
+
 }
