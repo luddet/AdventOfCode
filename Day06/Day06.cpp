8fb7d3abb51a9dfb1d06d1426699ebd6f7510087
luddet <ludde@tunel.se>
2020-12-06 12:32:22 +0100

    Day06 part 1 complete

--- Day06/Day06.cpp
+++ Day06/Day06.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <set>
 
 int main()
 {
+    std::ifstream ifs("input.txt");
+    std::string line;
+    uint32_t totalCount(0);
+    std::set<char> groupAnswers;
+    while (getline(ifs, line))
+    {
+        if (line.empty())
+        {
+            totalCount += groupAnswers.size();
+            groupAnswers.clear();
+            continue;
+        }
 
+        for (auto c : line)
+            groupAnswers.insert(c);
+    }
+
+    totalCount += groupAnswers.size();
+
+    std::cout << "Day06 Part 1: " << int(totalCount) << std::endl;
 }
