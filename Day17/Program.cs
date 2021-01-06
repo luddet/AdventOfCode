937d105124cbd813dde841689b8bbe6f2ac7dcf2
luddet <ludde@tunel.se>
2021-01-06 19:34:56 +0100

    stuff

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -11,8 +11,48 @@ namespace Day17
 		static void Main(string[] args)
 		{
 			const int steps = 356;
-			const int totalValues = 2017; // 50000000;
+			const int maxValue = 50000000;
 
+
+//			var (id1, id2) = ExecuteWithLinkedList(steps, 2017);
+			var (id1, id2) = ExecuteWithBufferArray(3, 2017);
+			
+			Console.WriteLine($"{id1}, {id2}");
+			Console.ReadLine();
+		}
+
+		public static (int idAfter2017, int idAfterZero) ExecuteWithBufferArray(int steps, int maxValue)
+		{
+			var buffer = new int[maxValue+1];
+			int currentPosition = 0;
+			for (int i = 1; i <= maxValue; ++i)
+			{
+				currentPosition = (currentPosition + steps) % i;
+				buffer[i] = buffer[currentPosition];
+				currentPosition = buffer[currentPosition] = i;
+
+				DebugPrintBuffer(i+1, buffer);
+				if (i > 10)
+					break;
+			}
+
+			return (buffer[2017], buffer[0]);
+		}
+
+		private static void DebugPrintBuffer(int count, int[] buffer)
+		{
+			int index = 0;
+			var sb = new StringBuilder(count);
+			for (int j = 0; j < count; ++j)
+			{
+				sb.Append(index);
+				index = buffer[index];
+			}
+			Console.WriteLine(sb.ToString());
+		}
+
+		public static (int idAfter2017, int idAfterZero) ExecuteWithLinkedList(int steps, int totalValues)
+		{
 			Node zeroNode = new Node(0);
 			zeroNode.Next = zeroNode;
 
@@ -36,9 +76,7 @@ namespace Day17
 
 			int idAfterZero = zeroNode.Next.Id;
 
-			Console.WriteLine($"Part 1: {idAfter2017}");
-			//Console.WriteLine($"Part 2: {idAfterZero}");
-			Console.ReadLine();
+			return (idAfter2017, idAfterZero);
 		}
 	}
 
