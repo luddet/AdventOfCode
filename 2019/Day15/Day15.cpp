dcb06bc12f38a86fcc1b1f52333636190c1f1e67
luddet <ludde@tunel.se>
2021-01-12 02:39:25 +0100

    Day15 Setup

---
+++ 2019/Day15/Day15.cpp
@@ -0,0 +1,22 @@
+// https://adventofcode.com/2019/day/15
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <functional>
+
+const char* EXAMPLE =
+"";
+
+int main()
+{
+	std::istringstream is(EXAMPLE);
+	//	std::ifstream is("input.txt");
+
+	std::cout << "Day15 Part1: " << std::endl;
+	std::cout << "Day15 Part2: " << std::endl;
+}
