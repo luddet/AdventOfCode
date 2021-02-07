c572b94fb702f7c2519f6339f43ccc47a5240468
luddet <luddet@users.noreply.github.com>
2021-02-07 17:57:34 +0100

    Day16 Part 2 Complete, decent speed. ~42 seconds.

--- 2019/Day16/Day16.cpp
+++ 2019/Day16/Day16.cpp
@@ -11,7 +11,28 @@
 #include <numeric>
 #include <chrono>
 
+class ScopeTimer
+{
+public:
+	ScopeTimer(std::chrono::duration<double>& duration):m_duration(duration)
+	{
+		m_start = std::chrono::steady_clock::now();
+	}
+
+	~ScopeTimer()
+	{
+		m_duration = std::chrono::steady_clock::now() - m_start;
+	}
+
+private:
+	std::chrono::duration<double>& m_duration;
+	std::chrono::steady_clock::time_point m_start;
+};
+
 const char* EXAMPLE1 = "12345678";
+const char* EXAMPLE2 = "80871224585914546619083218645595";
+const char* EXAMPLE3 = "123456789";
+const char* EXAMPLE4 = "03036732577212944063491565474664";
 
 template<typename T>
 std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
@@ -22,70 +43,64 @@ std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
 	return os;
 }
 
+
 std::vector<int64_t> transform(const std::vector<uint8_t>& list, size_t iterations)
 {
-	std::vector basePattern = { 0, 1, 0, -1 };
-	const size_t patternSize = basePattern.size();
-	const int64_t listSize = list.size();
-	std::vector<int64_t> listA(listSize);
-	std::vector<int64_t> listB(list.begin(), list.end());
-	auto* current = &listA;
-	auto* old = &listB;
+	const size_t listSize = list.size();
+	std::vector<int64_t> partials(listSize);
+	std::vector<int64_t> output(list.begin(), list.end());
 
-	std::vector<int64_t> factor{ 1,-1,-1, 1};
-	auto startTime = std::chrono::steady_clock::now();
-	auto lastPrint = startTime;
-	for (size_t iteration = 0; iteration < iterations; ++iteration)
+	std::chrono::duration<double> time;
 	{
-		std::fill(current->begin(), current->end(), 0);
-		current->back() = old->back();
-		for (int64_t outIndex = listSize - 2; outIndex >= 0; --outIndex)
-			(*current)[outIndex] = (*old)[outIndex] + (*current)[outIndex+1];
+		ScopeTimer timer(time);
 
-		int64_t factorIndex = 1;
-		int64_t step = 2;
-		for (int64_t start = listSize - 1; start >= 0; start /= 2)
+		auto startTime = std::chrono::steady_clock::now();
+		auto lastPrint = startTime;
+		for (size_t iteration = 0; iteration < iterations; ++iteration)
 		{
-			int64_t currentPartial{ 0 };
-			for (int64_t outIndex = start-1; outIndex >= 0; --outIndex)
+			std::partial_sum(output.begin(), output.end(), partials.begin());
+
 			{
-				int64_t oldStart = (outIndex+1) * step - 1;
-				int64_t oldIndex = std::min(oldStart + step - 1, listSize - 1);
-				currentPartial += factor[factorIndex] * (*old)[oldIndex--];
-
-				for (; oldIndex >= oldStart; --oldIndex)
-					currentPartial += factor[factorIndex] * (*old)[oldIndex];
-				(*current)[outIndex] += currentPartial;
+				int f = 1;
+				int64_t sum = 0;
+				for (size_t i = 0; i < listSize; i += 2, f = -f)
+					sum += f * output[i];
+				output[0] = abs(sum) % 10;
 			}
-			factorIndex = ++factorIndex % 4;
-			++step;
-		}
 
-		std::swap(current, old);
+			for (size_t i = 1; i < listSize; ++i)
+			{
+				size_t stepSize = 2 * (i + 1);
+				size_t startIndex = i;
+				size_t endIndex = startIndex + i;
+				int64_t factor = 1;
+				int64_t sum = 0;
+				for (; startIndex < listSize; startIndex += stepSize, endIndex += stepSize, factor = -factor)
+					sum += factor * (partials[std::min(endIndex, listSize - 1)] - partials[startIndex - 1]);
 
-		auto now = std::chrono::steady_clock::now();
-		[[maybe_unused]]
-		auto timeSinceLast = std::chrono::duration<double>(now - lastPrint).count();
+				output[i] = abs(sum) % 10;
+			}
 
-		[[maybe_unused]]
-		auto timeSinceStart = std::chrono::duration<double>(now - startTime).count();
+			auto now = std::chrono::steady_clock::now();
+			auto timeSinceLast = std::chrono::duration<double>(now - lastPrint).count();
+			auto timeSinceStart = std::chrono::duration<double>(now - startTime).count();
 
-		//if (timeSinceLast > 1)
-		{
-			//std::cout << "iteration: " << iteration
-			//	<< ", time/iteration (s): " << timeSinceStart / double_t(iteration+1) << std::endl;
-			std::cout << *old << std::endl;
-			lastPrint = now;
-
+			if (timeSinceLast > 5)
+			{
+				std::cout << "iteration: " << iteration
+					<< ", time/iteration (s): " << timeSinceStart / double_t(iteration + 1) << std::endl;
+				lastPrint = now;
+			}
 		}
 	}
-	return *old;
+	std::cout << "total time (s): " << time.count() << std::endl;
+	return output;
 }
 
 int main()
 {
-	std::istringstream is(EXAMPLE1);
-	//std::ifstream is("input.txt");
+	//std::istringstream is(EXAMPLE4);
+	std::ifstream is("input.txt");
 
 	std::string input;
 	std::getline(is, input);
@@ -95,29 +110,26 @@ int main()
 	for (auto c : input)
 		list.push_back(c - '0');
 
-	//auto part1Transformed = transform(list, 100);
+	auto part1Transformed = transform(list, 100);
 
-	//std::vector<int32_t> finalList{};
-	//std::transform(part1Transformed.begin(), part1Transformed.begin() + 8, std::back_inserter(finalList), [](auto e) { return e + '0'; });
+	std::vector<char> finalList{};
+	std::transform(part1Transformed.begin(), part1Transformed.begin() + 8, std::back_inserter(finalList), [](auto e) -> char { return (char)e + '0'; });
 
-	const size_t repeats{ 1 };
+	size_t offset = 0;
+	for (size_t i = 0; i < 7; ++i)
+		offset += size_t(std::pow(10, i)) * list[6 - i];
+
+	const size_t repeats{ 10000 };
 	std::vector<uint8_t> part2Input(list.size() * repeats);
 	auto dest = part2Input.begin();
 	for (size_t i = 0; i < repeats; ++i)
 		dest = std::copy(list.begin(), list.end(), dest);
 
-	std::cout << std::vector<int32_t>(part2Input.begin(), part2Input.end()) << std::endl;
 	auto part2Transformed = transform(part2Input, 100);
-	
-	size_t offset = 0;
-	for (size_t i = 0; i < 7; ++i)
-		offset += size_t(std::pow(10, i)) * part2Transformed[6 - i];
 
-	std::vector<int32_t> part2Final{};
-	//std::transform(part2Transformed.begin() + offset, part2Transformed.begin() +offset + 8, std::back_inserter(part2Final), [](auto e) { return e + '0'; });
+	std::vector<char> part2Final{};
+	std::transform(part2Transformed.begin() + offset, part2Transformed.begin() +offset + 8, std::back_inserter(part2Final), [](auto e) -> char { return (char)e + '0'; });
 
-
-
-	//std::cout << "Day16 Part1: " << finalList << std::endl;
-	//std::cout << "Day16 Part2: " << part2Final << std::endl;
+	std::cout << "Day16 Part1: " << finalList << std::endl;
+	std::cout << "Day16 Part2: " << part2Final << std::endl;
 }
