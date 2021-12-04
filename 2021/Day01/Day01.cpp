58185e62ad98e9b630cb084e16b6df1f0bb8211a
luddet <luddet@users.noreply.github.com>
2021-12-04 23:14:38 +0100

    Day01 part 1 complete

--- 2021/Day01/Day01.cpp
+++ 2021/Day01/Day01.cpp
@@ -1,8 +1,21 @@
 // https://adventofcode.com/2021/day/1
 
 #include <iostream>
+#include <fstream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::ifstream is("input.txt");
+    uint32_t lastDepth, currentDepth;
+    uint32_t numIncrements(0);
+
+    is >> lastDepth;
+    while (!is.eof())
+    {
+        is >> currentDepth;
+        if (currentDepth > lastDepth)
+            ++numIncrements;
+        lastDepth = currentDepth;
+    }
+    std::cout << "Day01 part 1: " << numIncrements << std::endl;
 }
