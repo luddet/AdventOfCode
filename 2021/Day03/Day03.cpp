bfa4ea8a8219b9276004268a713b3f629d54d067
luddet <luddet@users.noreply.github.com>
2022-08-07 21:48:45 +0200

    Tabified Day03.cpp

--- 2021/Day03/Day03.cpp
+++ 2021/Day03/Day03.cpp
@@ -11,91 +11,91 @@ constexpr size_t NUM_BITS(12);
 
 int part1(const std::vector<std::string>& lines)
 {
-    uint32_t numOnes[NUM_BITS]{ 0,0,0,0,0,0,0,0,0,0,0,0 };
+	uint32_t numOnes[NUM_BITS]{ 0,0,0,0,0,0,0,0,0,0,0,0 };
 
-    for (auto& line : lines)
-    {
-        assert(line.length() == NUM_BITS);
-        for (size_t i = 0; i < line.length(); ++i)
-        {
-            if (line[i] == '1')
-                ++numOnes[i];
-        }
-    }
+	for (auto& line : lines)
+	{
+		assert(line.length() == NUM_BITS);
+		for (size_t i = 0; i < line.length(); ++i)
+		{
+			if (line[i] == '1')
+				++numOnes[i];
+		}
+	}
 
-    uint16_t gamma(0);
-    size_t numLines(lines.size());
-    for (size_t i = 0; i < NUM_BITS; ++i)
-    {
-        if (numOnes[i] > numLines - numOnes[i])
-            gamma |= 1 << (NUM_BITS - i - 1);
-    }
-    uint16_t epsilon = ~gamma & 0b0000111111111111;
-    return epsilon * gamma;
+	uint16_t gamma(0);
+	size_t numLines(lines.size());
+	for (size_t i = 0; i < NUM_BITS; ++i)
+	{
+		if (numOnes[i] > numLines - numOnes[i])
+			gamma |= 1 << (NUM_BITS - i - 1);
+	}
+	uint16_t epsilon = ~gamma & 0b0000111111111111;
+	return epsilon * gamma;
 }
 
 int part2(const std::vector<std::string>& lines)
 {
-    std::vector<uint16_t> numbers(lines.size());
-    for (auto& line : lines)
-        numbers.push_back(std::stoi(line, nullptr, 2));
+	std::vector<uint16_t> numbers(lines.size());
+	for (auto& line : lines)
+		numbers.push_back(std::stoi(line, nullptr, 2));
 
-    uint32_t numOnes[NUM_BITS]{ 0,0,0,0,0,0,0,0,0,0,0,0 };
-    uint32_t numZeros[NUM_BITS]{ 0,0,0,0,0,0,0,0,0,0,0,0 };
+	uint32_t numOnes[NUM_BITS]{ 0,0,0,0,0,0,0,0,0,0,0,0 };
+	uint32_t numZeros[NUM_BITS]{ 0,0,0,0,0,0,0,0,0,0,0,0 };
 
-    for (auto n : numbers)
-    {
-        for (size_t bit = 0; bit < NUM_BITS; ++bit)
-        {
-            if ((n & (1 << bit)) != 0)
-                ++numOnes[bit];
-            else
-                ++numZeros[bit];
-        }
-    }
+	for (auto n : numbers)
+	{
+		for (size_t bit = 0; bit < NUM_BITS; ++bit)
+		{
+			if ((n & (1 << bit)) != 0)
+				++numOnes[bit];
+			else
+				++numZeros[bit];
+		}
+	}
 
-    uint16_t o2_criteria(0);
-    uint16_t co2_criteria(0);
+	uint16_t o2_criteria(0);
+	uint16_t co2_criteria(0);
 
-    for (size_t bit = 0; bit < NUM_BITS; ++bit)
-    {
-        o2_criteria |= (numOnes[bit] >= numZeros[bit]) ? 1 << bit : 0;
-        co2_criteria |= (numOnes[bit] < numZeros[bit]) ? 1 << bit : 0;
-    }
+	for (size_t bit = 0; bit < NUM_BITS; ++bit)
+	{
+		o2_criteria |= (numOnes[bit] >= numZeros[bit]) ? 1 << bit : 0;
+		co2_criteria |= (numOnes[bit] < numZeros[bit]) ? 1 << bit : 0;
+	}
 
-    auto o2nums(numbers);
-    for (size_t bit = 0; bit < NUM_BITS; ++bit)
-    {
-        uint16_t bitMask = o2_criteria & (1 << bit);
-        uint16_t numErased(0);
-        std::erase_if(o2nums, [&o2nums, bitMask, &numErased](uint16_t n) {
-            bool erase = (n & bitMask) == 0 && o2nums.size() > numErased + 1;
-            if (erase)
-                ++numErased;
-            return erase;
-        });
-        if (o2nums.size() == 1)
-            break;
-    }
+	auto o2nums(numbers);
+	for (size_t bit = 0; bit < NUM_BITS; ++bit)
+	{
+		uint16_t bitMask = o2_criteria & (1 << bit);
+		uint16_t numErased(0);
+		std::erase_if(o2nums, [&o2nums, bitMask, &numErased](uint16_t n) {
+			bool erase = (n & bitMask) == 0 && o2nums.size() > numErased + 1;
+			if (erase)
+				++numErased;
+			return erase;
+		});
+		if (o2nums.size() == 1)
+			break;
+	}
 
-    auto co2nums(numbers);
-    for (size_t bit = 0; bit < NUM_BITS; ++bit)
-    {
-        uint16_t bitMask = co2_criteria & (1 << bit);
-        std::erase_if(co2nums, [&co2nums, bitMask](uint16_t n) {
-            return (n & bitMask) == 0 && co2nums.size() > 1;
-        });
-        if (co2nums.size() == 1)
-            break;
-    }
+	auto co2nums(numbers);
+	for (size_t bit = 0; bit < NUM_BITS; ++bit)
+	{
+		uint16_t bitMask = co2_criteria & (1 << bit);
+		std::erase_if(co2nums, [&co2nums, bitMask](uint16_t n) {
+			return (n & bitMask) == 0 && co2nums.size() > 1;
+		});
+		if (co2nums.size() == 1)
+			break;
+	}
 
-    return o2nums[0] * co2nums[0];
+	return o2nums[0] * co2nums[0];
 }
 
 
 int main()
 {
-    auto lines(readLines("input.txt"));
-    std::cout << "Day03 part 1: " << part1(lines) << std::endl;
-    std::cout << "Day03 part 2: " << part2(lines) << std::endl;
+	auto lines(readLines("input.txt"));
+	std::cout << "Day03 part 1: " << part1(lines) << std::endl;
+	std::cout << "Day03 part 2: " << part2(lines) << std::endl;
 }
