8de3f10b45aa48248743e8b2dc4309ba5ad6abff
luddet <luddet@users.noreply.github.com>
2022-08-31 20:52:05 +0200

    Added readItems()

--- 2021/Utilities/utilities.h
+++ 2021/Utilities/utilities.h
@@ -5,5 +5,18 @@
 
 #include "dllexport.h"
 
+template<typename T>
+auto readItems(std::istream& os)
+{
+	std::vector<T> result;
+	while (!os.eof())
+	{
+		T i;
+		os >> i;
+		result.push_back(i);
+	}
+	return result;
+}
+
 DLLEXPORT std::vector<int> readInts(std::istream& os);
 DLLEXPORT std::vector<std::string> readLines(const std::string& filePath);
