4b0a85f63dc467d23bfd59343b19cab5bc320260
luddet <luddet@users.noreply.github.com>
2022-09-03 14:01:49 +0200

    Day08 WIP

--- 2021/Day08/Day08.cpp
+++ 2021/Day08/Day08.cpp
@@ -1,11 +1,23 @@
 #include <algorithm>
+#include <array>
+#include <bitset>
 #include <fstream>
 #include <iostream>
 #include <numeric>
+#include <ranges>
 #include <string>
+#include <string_view>
+#include <unordered_map>
 #include <vector>
 
+
 #include "../Utilities/utilities.h"
+
+using namespace std::string_literals;
+namespace ranges = std::ranges;
+
+constexpr size_t NUM_BITS = 7;
+using bits_t = std::bitset<NUM_BITS>;
 
 auto readTokens(std::istream& is, auto& container, auto num)
 {
@@ -21,10 +33,28 @@ auto readTokens(std::istream& is, auto& container, auto num)
 	container.push_back(std::move(input));
 };
 
+[[nodiscard]] bits_t makeBitset(const std::string_view& pattern)
+{
+	return std::accumulate(begin(pattern), end(pattern), bits_t{}, [](auto bits, char ch)
+	{
+		bits.set(ch - 'a', true);
+		return bits;
+	});
+}
+
+template<class Container>
+auto pop_if(Container& container, auto pred) -> Container::value_type
+{
+	auto it = ranges::find_if(container, pred);
+	Container::value_type result = *it;
+	container.erase(it);
+	return result;
+
+}
 
 int main()
 {
-	std::ifstream ifs("input.txt");
+	std::ifstream ifs("test_input.txt");
 	
 	std::vector<std::vector<std::string>> inputs;
 	std::vector<std::vector<std::string>> outputs;
@@ -44,6 +74,23 @@ int main()
 			return len == 2 || len == 3 || len == 4 || len == 7;
 		});
 	});
+
+	for (size_t i = 0; i < inputs.size(); ++i)
+	{
+		std::vector<bits_t> inputBits, outputBits;
+		
+		ranges::transform(inputs[i], std::back_inserter(inputBits), makeBitset);
+		ranges::transform(outputs[i], std::back_inserter(outputBits), makeBitset);
+
+		std::array<bits_t, 10> bitpatterns{};
+
+		bitpatterns[1] = pop_if(inputBits, [](auto& bits) { return bits.count() == 2; });
+		bitpatterns[7] = pop_if(inputBits, [](auto& bits) { return bits.count() == 3; });
+		bitpatterns[4] = pop_if(inputBits, [](auto& bits) { return bits.count() == 4; });
+		bitpatterns[8] = pop_if(inputBits, [](auto& bits) { return bits.count() == 7; });
+
+	}
+
 
 	std::cout << "Day08 Part 1: " << count << '\n';
 	std::cout << "Day08 Part 2: " << '\n';
