a494bb4aa2895487c097fb1695404d0822b98160
luddet <luddet@users.noreply.github.com>
2022-09-03 16:25:27 +0200

    Day09 Setup

---
+++ 2021/Day09/Day09.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+#include <iostream>
+
+#include "../Utilities/utilities.h"
+
+int main()
+{
+	std::ifstream ifs("input.txt");
+
+
+	std::cout << "Day09 Part 1: " << '\n';
+	std::cout << "Day09 Part 2: " << '\n';
+}
