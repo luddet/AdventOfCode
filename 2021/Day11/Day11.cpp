20d9d5b8ff5a91620dc7a618cfb40294d6826390
luddet <luddet@users.noreply.github.com>
2022-09-10 14:28:06 +0200

    Day11 Setup

---
+++ 2021/Day11/Day11.cpp
@@ -0,0 +1,11 @@
+#include <fstream>
+#include <iostream>
+
+int main()
+{
+	std::ifstream ifs("test_input.txt");
+
+
+	std::cout << "Day11 Part 1: " << '\n';
+	std::cout << "Day11 Part 2: " << '\n';
+}
