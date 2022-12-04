fb2d0a04f918383a151eb86bc726a1e6e52e0ab5
luddet <luddet@users.noreply.github.com>
2022-12-04 18:08:26 +0100

    Simplified parsing

--- 2022/Day04/Day04.cpp
+++ 2022/Day04/Day04.cpp
@@ -10,22 +10,19 @@ int main()
 {
 	auto lines{ readLines("input.txt") };
 
-	const std::regex delimiters("[-,]");
+	const std::regex pattern(R"((\d+)-(\d+),(\d+)-(\d+))");
 	int part1{}, part2{};
 
 	for(const auto& line : lines)
 	{
 		std::array<int, 4> n{};
-		std::transform(std::sregex_token_iterator(begin(line), end(line), delimiters, -1), std::sregex_token_iterator(), begin(n), [](const std::string& match)
-		{
-			return std::atoi(match.c_str());
-		});
+		std::smatch m;
+		std::regex_match(line, m, pattern);
+		std::transform(m.begin() + 1, m.end(), n.begin(), [](auto submatch) { return std::atoi(submatch.str().c_str()); });
 
-		// full overlap
 		if (n[0] <= n[2] && n[1] >= n[3] || n[0] >= n[2] && n[1] <= n[3])
 			++part1;
 
-		// some overlap
 		if (n[0] <= n[2] && n[1] >= n[2] || n[0] <= n[3] && n[1] >= n[3] || n[2] <= n[0] && n[3] >= n[0] || n[2] <= n[1] && n[3] >= n[1])
 			++part2;
 	}
