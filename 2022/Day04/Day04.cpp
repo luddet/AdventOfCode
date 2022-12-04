3856cff8cf298a537c4adab2b188077096a54d11
luddet <luddet@users.noreply.github.com>
2022-12-04 17:12:30 +0100

    Day04 Part2 Complete

--- 2022/Day04/Day04.cpp
+++ 2022/Day04/Day04.cpp
@@ -13,20 +13,28 @@
 
 int main()
 {
-
 	auto lines{ readLines("input.txt") };
 
-	auto part1 = std::count_if(cbegin(lines), cend(lines),[](const auto& line) {
-		std::array<int, 4> n{};
-		std::regex r(R"(\d+)");
+	int part1{}, part2{};
+	std::array<int, 4> n{};
+	const std::regex r(R"(\d+)");
+
+	for(const auto& line : lines)
+	{
 		std::transform(std::sregex_token_iterator(begin(line), end(line), r), std::sregex_token_iterator(), begin(n), [](const std::string& match)
 		{
 			return std::atoi(match.c_str());
 		});
-		return n[0] <= n[2] && n[1] >= n[3] || n[0] >= n[2] && n[1] <= n[3];
-	});
 
+		// full overlap
+		if (n[0] <= n[2] && n[1] >= n[3] || n[0] >= n[2] && n[1] <= n[3])
+			++part1;
+
+		// some overlap
+		if (n[0] <= n[2] && n[1] >= n[2] || n[0] <= n[3] && n[1] >= n[3] || n[2] <= n[0] && n[3] >= n[0] || n[2] <= n[1] && n[3] >= n[1])
+			++part2;
+	}
 
 	std::cout << "Day04 Part 1: " << part1 << '\n';
-	std::cout << "Day04 Part 2: " << '\n';
+	std::cout << "Day04 Part 2: " << part2 << '\n';
 }
