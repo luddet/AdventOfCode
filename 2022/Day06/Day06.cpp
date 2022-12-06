02381e5f713dfd360dbb3997d3b313a96c7faf97
luddet <luddet@users.noreply.github.com>
2022-12-06 10:54:11 +0100

    Day06 Setup

---
+++ 2022/Day06/Day06.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+#include <iostream>
+
+
+int main()
+{
+
+	std::ifstream ifs("input.txt");
+
+
+	std::cout << "Day06 Part 1: " << '\n';
+	std::cout << "Day06 Part 2: " << '\n';
+}
