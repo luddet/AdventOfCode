9fa98b067381010973c7a20e776e801351fafb91
luddet <luddet@users.noreply.github.com>
2022-12-29 18:37:05 +0100

    Day10 Setup

---
+++ 2022/Day10/Day10.cpp
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <iostream>
+
+#include "../../2021/Utilities/utilities.h"
+
+int main()
+{
+	auto input = getInput(2022, 10);
+
+	std::cout << input << "\n";
+
+	std::cout << "Day10 Part 1: " << '\n';
+	std::cout << "Day10 Part 2: " << '\n';
+}
