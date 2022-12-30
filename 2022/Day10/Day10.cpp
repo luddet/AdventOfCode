e783b28cff24ca4033d186ceeb6ba53b158722f1
luddet <luddet@users.noreply.github.com>
2022-12-30 18:36:00 +0100

    Day10 Part1 Complete

--- 2022/Day10/Day10.cpp
+++ 2022/Day10/Day10.cpp
@@ -1,14 +1,37 @@
-#include <fstream>
 #include <iostream>
+#include <string>
 
 #include "../../2021/Utilities/utilities.h"
 
 int main()
 {
-	auto input = getInput(2022, 10);
+	auto input = split(getInput(2022, 10));
+	int x{ 1 };
+	size_t ic{ 0 };
+	int cycle{ 1 };
+	int signalSum{ 0 };
+	for (auto ip = input.cbegin(); ip != input.cend(); )
+	{
+		if (cycle % 40 == 20)
+			signalSum += cycle * x;
 
-	std::cout << input << "\n";
+		if (ip->starts_with("addx"))
+		{
+			if (ic == 0)
+				ic = 2;
+			
+			if (--ic == 0)
+			{
+				int operand = std::stoi(ip->substr(5));
+				x += operand;
+			}
+		}
 
-	std::cout << "Day10 Part 1: " << '\n';
+		++cycle;
+		if (ic == 0) // get new instruction
+			++ip;
+	}
+	
+	std::cout << "Day10 Part 1: " << signalSum << '\n';
 	std::cout << "Day10 Part 2: " << '\n';
 }
