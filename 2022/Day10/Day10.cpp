34c9da09d3c0125eec848bf47268c598ed30ffd5
luddet <luddet@users.noreply.github.com>
2022-12-31 18:31:30 +0100

    DAy10 Part2 Complete

--- 2022/Day10/Day10.cpp
+++ 2022/Day10/Day10.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <iostream>
 #include <string>
 
@@ -6,14 +7,21 @@
 int main()
 {
 	auto input = split(getInput(2022, 10));
+	
+	std::array<std::string, 6> display{};
 	int x{ 1 };
 	size_t ic{ 0 };
 	int cycle{ 1 };
 	int signalSum{ 0 };
+
 	for (auto ip = input.cbegin(); ip != input.cend(); )
 	{
 		if (cycle % 40 == 20)
 			signalSum += cycle * x;
+
+		int pos = (cycle-1) % 40;
+		int row = (cycle-1) / 40;
+		display[row] += (abs(pos - x) <= 1) ? '#' : ' ';
 
 		if (ip->starts_with("addx"))
 		{
@@ -34,4 +42,7 @@ int main()
 	
 	std::cout << "Day10 Part 1: " << signalSum << '\n';
 	std::cout << "Day10 Part 2: " << '\n';
+	for (const auto& line : display)
+		std::cout << line << '\n';
+
 }
