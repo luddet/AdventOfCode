d9abd647e1668878dfe4ee30a8fb05ffc2e71a06
luddet <luddet@users.noreply.github.com>
2023-01-02 16:07:34 +0100

    Day12 Setup

---
+++ 2022/Day12/Day12.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../../2021/Utilities/utilities.h"
+
+int main()
+{
+	auto input = getInput(2022, 12);
+
+
+	std::cout << "Day12 Part 1: " << '\n';
+	std::cout << "Day12 Part 2: " << '\n';
+	std::cout << input;
+}
