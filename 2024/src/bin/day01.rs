40d3331023403ecaaa15d1581bdfb09cd513fdc0
luddet <ludde@tunel.se>
2024-12-01 23:07:04 +0100

    Day01 Part 2 complete.

--- 2024/src/bin/day01.rs
+++ 2024/src/bin/day01.rs
@@ -8,19 +8,33 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let input = fs::read_to_string(path).expect("day01");
 
-    let mut lists: Vec<Vec<i32>> = vec![Vec::new(), Vec::new()];
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
 
     for line in input.lines() {
         let parts:Vec<&str> = line.split_whitespace().take(2).collect();
-        lists[0].push(parts[0].parse()?);
-        lists[1].push(parts[1].parse()?);
+        left.push(parts[0].parse()?);
+        right.push(parts[1].parse()?);
     }
 
-    for list in &mut lists {
-        list.sort();
-    }
+    left.sort();
+    right.sort();
 
-    let sum: i32 = lists[0].iter().zip(&lists[1]).map(|(l, r)| (r - l).abs()).sum();
-    println!("Day01 part 1: {sum}");
+    println!("Day01 part 1: {}", part1(&left, &right));
+    println!("Day01 part 2: {}", part2(&left, &right));
+
     Ok(())
 }
+
+fn part1(l: &Vec<i32>, r: &Vec<i32>) -> i32 {
+    l.iter().zip(r).map(|(l, r)| (r - l).abs()).sum()
+}
+
+fn part2(l: &Vec<i32>, r: &Vec<i32>) -> i32 {
+    let mut sum: usize = 0;
+    for n in l {
+        sum += *n as usize * r.into_iter().filter(|r_n| n == *r_n).count();
+    }
+    assert!(sum <= i32::MAX as usize);
+    sum as i32
+}   
