1e118eac641e64f28f33ad231fce4062835a424c
luddet <ludde@tunel.se>
2024-12-03 23:04:56 +0100

    Day02 part 2 complete

--- 2024/src/bin/day02.rs
+++ 2024/src/bin/day02.rs
@@ -1,6 +1,6 @@
-use std::path::Path;
+use std::{error::Error, path::Path};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let input_dir = std::env::var("AOC_INPUT").expect("Failed to find env var AOC_INPUT");
     let input_dir = Path::new(&input_dir);
     let input_file = input_dir.join("2024").join("day02.txt");
@@ -14,11 +14,46 @@ fn main() {
     )
     .collect();
 
-    let count = input.iter()
-        .filter(|line|
-            line.is_sorted_by(|a, b| b > a && b - a <= 3)
-            || line.is_sorted_by(|a, b| b < a && a - b <= 3)
-        ).count();
+    let part1 = input.iter()
+        .filter(|line| is_report_safe_1(*line))
+        .count();
 
-    println!("Day02 part 1: {count}");
+    let part2 = input.iter()
+        .filter(|line| is_report_safe_2(*line))
+        .count();
+    
+    println!("Day02 part 1: {part1}");
+    println!("Day02 part 2: {part2}");
+
+    Ok(())
+}
+
+fn compare_inc(a:&i32, b:&i32) -> bool {
+    a < b && b - a <= 3
+}
+
+fn compare_dec(a:&i32, b:&i32) -> bool {
+    a > b && a - b <= 3
+}
+
+fn is_report_safe_1(report: &Vec<i32>) -> bool {
+    report.iter().is_sorted_by(|a,b| compare_inc(a, b)) ||
+    report.iter().is_sorted_by(|a,b| compare_dec(a, b))
+}
+
+fn is_report_safe_2(report: &Vec<i32>) -> bool {
+    if is_report_safe_1(&report) {
+        return true;
+    }
+
+    for i in 0..report.len() {
+        let excl_iter = report.iter().take(i).chain(report.iter().skip(i+1));
+        if excl_iter.to_owned().is_sorted_by(|a,b| compare_inc(a, b))
+            || excl_iter.is_sorted_by(|a,b| compare_dec(a, b))
+        {
+            return true;
+        }
+    }
+
+    false
 }
