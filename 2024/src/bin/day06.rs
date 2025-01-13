7316a60f3a14c3c3b136c67c351ca3106dd5ce64
luddet <ludde@tunel.se>
2025-01-13 22:30:01 +0100

    Day06 part 2 complete.

--- 2024/src/bin/day06.rs
+++ 2024/src/bin/day06.rs
@@ -1,10 +1,9 @@
 use std::{collections::HashSet, ops::Add};
-
 use year2024::input::get_input_as_string;
 
 type Map = Vec<Vec<char>>;
 
-#[derive(Default, Hash, Debug, Eq, PartialEq, Clone)]
+#[derive(Default, Hash, Debug, Eq, PartialEq, Clone, Copy)]
 struct Vector2 {
     x: i32,
     y: i32,
@@ -38,7 +37,7 @@ impl<'a> Add for &'a Vector2 {
     }
 }
 
-#[derive(Default, Debug)]
+#[derive(Default, Debug, Hash, Clone, Eq, PartialEq)]
 enum Direction {
     #[default]
     UP,
@@ -47,7 +46,7 @@ enum Direction {
     RIGHT,
 }
 
-#[derive(Default, Debug)]
+#[derive(Default, Debug, Hash, Clone, Eq, PartialEq)]
 struct Guard {
     pos: Vector2,
     dir: Direction,
@@ -67,26 +66,25 @@ fn move_guard(guard: &mut Guard, map: &Map, map_size: &Vector2) -> bool {
         return false;
     }
 
-    if map[new_pos.y as usize][new_pos.x as usize] == '.' {
-        guard.pos = new_pos;
-    } else {
-        guard.dir = new_dir;
+    match map[new_pos.y as usize][new_pos.x as usize] {
+        '.' | '^' => guard.pos = new_pos,
+        _ => guard.dir = new_dir,
     }
 
     true
 }
 
 fn coord_in_bounds(coord: &Vector2, bounds: &Vector2) -> bool {
-    coord.x >= 0 && coord.x <= bounds.x && coord.y >= 0 && coord.y <= bounds.y
+    coord.x >= 0 && coord.x <= bounds.x - 1 && coord.y >= 0 && coord.y <= bounds.y - 1
 }
 
 fn main() {
-    let mut input = get_input_as_string(2024, 6).unwrap();
+    let input = get_input_as_string(2024, 6).unwrap();
 
-    let map: Map = Vec::from_iter(input.lines().map(|line| Vec::from_iter(line.chars())));
+    let mut map: Map = Vec::from_iter(input.lines().map(|line| Vec::from_iter(line.chars())));
     let map_size = Vector2::new(map[0].len() as i32, map.len() as i32);
     let mut guard = Guard::default();
-    let mut visited: HashSet<Vector2> = HashSet::new();
+    let mut part1_visited: HashSet<Vector2> = HashSet::new();
 
     'outer: for (y, line) in map.iter().enumerate() {
         for (x, chr) in line.iter().enumerate() {
@@ -96,13 +94,36 @@ fn main() {
             }
         }
     }
-    input = input.replace('^', ".");
 
-    dbg!(&guard);
-    visited.insert(guard.pos.clone());
+    let start_pos = guard.pos;
+
+    // Part 1
     while move_guard(&mut guard, &map, &map_size) {
-        visited.insert(guard.pos.clone());
+        part1_visited.insert(guard.pos);
+    }
+    let part1_num_pos = 1 /* start pos */ + part1_visited.len();
+
+    // Part 2
+    let mut part2_visited: HashSet<Guard> = HashSet::new();
+    let mut loop_count = 0;
+    for state in part1_visited.iter() {
+        map[state.y as usize][state.x as usize] = '#';
+
+        guard.dir = Direction::UP;
+        guard.pos = start_pos;
+
+        // // Check for loop
+        part2_visited.clear();
+        while move_guard(&mut guard, &map, &map_size) {
+            if !part2_visited.insert(guard.clone()) {
+                loop_count += 1;
+                break;
+            }
+        }
+
+        map[state.y as usize][state.x as usize] = '.';
     }
 
-    println!("Day06 part 1: {}", visited.len());
+    println!("Day06 part 1: {}", part1_num_pos);
+    println!("Day06 part 2: {loop_count}");
 }
